75fc28e57e96b5c14256f6454346377e0006f4d7
SID37 <1999login@mail.ru>
2023-07-08 22:12:35 +0300

    Add level-1.3

---
+++ Assets/Scripts/Rose.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rose : MonoBehaviour
+{
+    public Animator animator;
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Enabled")) return;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
