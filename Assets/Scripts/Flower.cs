cfcd0e504f3efe9702e0e66415e667babc24bacd
SID37 <1999login@mail.ru>
2023-07-09 15:03:33 +0300

    Implement flower

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -10,6 +10,7 @@ public class Flower : MonoBehaviour
     void OnCollisionStay2D(Collision2D collision)
     {
         if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Enabled")) return;
+        animator.SetTrigger("Jump");
         var player = collision.collider.GetComponent<CharacterController>();
         player.Jump(JumpMultiplier, transform.TransformDirection(Vector3.up));
     }
